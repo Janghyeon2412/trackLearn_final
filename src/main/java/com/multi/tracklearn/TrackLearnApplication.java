7aa5a510d251b4eed098efae85b88a3ca0bf86a4
gahyeon <janghyeon2412@gmail.com>
2025-05-14 11:14:25 +0900

    프로젝트 초기 설정 - Gradle, 디렉토리 구조, 기본 설정

---
+++ src/main/java/com/multi/tracklearn/TrackLearnApplication.java
@@ -0,0 +1,13 @@
+package com.multi.tracklearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/multi/tracklearn/TrackLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.tracklearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
